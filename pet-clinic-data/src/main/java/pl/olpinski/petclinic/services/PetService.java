fcaf50d888d3be24c4ca0170ebcaa5ebf9e7bf85
Olgierd Olpiński <o.olpinski@gmail.com>
2020-12-17 11:54:45 +0100

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/pl/olpinski/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package pl.olpinski.petclinic.services;
+
+import pl.olpinski.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/pl/olpinski/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package pl.olpinski.petclinic.services;
+
+import pl.olpinski.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

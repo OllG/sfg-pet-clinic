82138145a89cc1c9c04fed85d436e50f49eee3f4
Olgierd Olpiński <o.olpinski@gmail.com>
2020-12-25 08:14:43 +0100

    #46

---
+++ pet-clinic-data/src/main/java/pl/olpinski/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package pl.olpinski.petclinic.services;
+
+import pl.olpinski.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/pl/olpinski/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package pl.olpinski.petclinic.services.map;
+
+import pl.olpinski.petclinic.model.Visit;
+import pl.olpinski.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visist");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
